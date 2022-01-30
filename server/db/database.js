4ff3719aed5b26f871341620ddc55005d4d06b42
Vitor Hugo <vitor4oliveira@gmail.com>
2022-01-30 15:19:28 -0300

    Functions(upadate, delete, insert and select) that interact with database was implemented.

--- server/db/database.js
+++ server/db/database.js
@@ -1,3 +1,4 @@
+const { text } = require('body-parser')
 const { Pool } = require('pg')
 const pool = new Pool({
     user: 'me',
@@ -10,9 +11,49 @@ const pool = new Pool({
 
 function getAllTasks(request, response){
     pool.query('SELECT * FROM list', (err, result) => {
-        if(err) throw `Ocorreu um erro ao consultar as tarefas: ${err.message}`
-        else response.send(result.rows)
+        if(err) throw `There was an error querying tasks: ${err.message}`
+        else response.status(200).send(result.rows)
     })
 }
 
-module.exports = { getAllTasks}
+function getLastTaskInserted(){
+    pool.query('SELECT MAX(id) FROM list', (err, result) => {
+        if(err) throw err
+        else result.rows[0].id
+    })
+}
+
+function insertTask(request, response){
+    const {text, concluded} = request.body
+    pool.query('INSERT INTO List (text, concluded) VALUES ($1, $2)', [text, concluded], (err, result) => {
+        if(err) throw `There was an error insert the task: ${err.message}`
+        else{
+            const id = getLastTaskInserted()
+            response.status(200).send([id, text, concluded])
+        } 
+        
+    })
+}
+
+function updatedTask(request, response){
+    const id = parseInt(request.params.id)
+    const [text, concluded] = request.body
+
+    pool.query('UPDATE list set text=$1, concluded=$2 where ID=$3', [text,concluded, id], (err, result) => {
+        if(err) throw `There was an error update the task: ${err.message}`
+        else response.status(200).send([id, text, concluded])
+    })
+}
+
+function deleteTask(request, response){
+    const id = parseInt(request.params.id)
+
+    pool.query('DELETE FROM list WHERE ID=$1', [id], (err, result) => {
+        if(err) throw `There was an error delete the task: ${err.message}`
+        else response.status(200).send('Task was deleted!')
+    })
+}
+
+
+
+module.exports = { getAllTasks, insertTask, updatedTask, deleteTask}
--- server/server.js
+++ server/server.js
@@ -5,12 +5,22 @@ const database = require('./db/database')
 
 
 app.use(express.static('./public'))
+
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true}))
 
 
 //Search tasks
 app.get('/tasks', database.getAllTasks)
 
+//Insert task
+app.post('/tasks', database.insertTask)
+
+//Update task
+app.put('/tasks:id', database.updatedTask)
+
+//Delete task
+app.delete('/tasks:id', database.deleteTask)
 
 
 
